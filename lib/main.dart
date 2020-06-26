0d477bb0fdcd67d76bab49e4f1d6d68058c0cfad
didierganthier <45739963+didierganthier@users.noreply.github.com>
2020-06-25 22:26:52 -0400

    Design updated/ Payment working

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_rave/flutter_rave.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,39 +10,15 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'FlutterWave',
+      theme: ThemeData(primaryColor: Colors.orange),
+      home: MyHomePage(title: 'FlutterWave'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -50,68 +27,86 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
+        centerTitle: true,
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+        child: Builder(
+          builder: (context) => SingleChildScrollView(
+            child: Padding(
+              padding: EdgeInsets.only(left: 10.0, right: 10),
+              child: InkWell(
+                onTap: ()=> _pay(context),
+                child: Card(
+                  color: Colors.orangeAccent,
+                  elevation: 15,
+                  child: Container(
+                    height: 250,
+                    width: MediaQuery.of(context).size.width,
+                    child: Center(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Text(
+                            "Card Payment",
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold
+                            ),
+                          ),
+                          SizedBox(
+                            width: 10,
+                          ),
+                          Icon(
+                            Icons.payment,
+                            color: Colors.black,
+                            size: 30,
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+
+  _pay(BuildContext context){
+    final snackBar_onFailure = SnackBar(content: Text("Transaction failed"));
+    final snackBar_onClosed = SnackBar(content: Text("Transaction closed"));
+    final _rave = RaveCardPayment(
+      isDemo: true,
+      encKey: "7fab82b347f5c9714fd377d8",
+      publicKey: "FLWPUBK-1bd6903f58e093d7bbdc9d1c38df6078-X",
+      transactionRef: "hvHPvKYaRuJLlJWSPWGGKUyaAfWeZKnm",
+      amount: 100,
+      email: "didierganthierperan@gmail.com",
+      onSuccess: (response){
+        if(mounted){
+          Scaffold.of(context).showSnackBar(
+            SnackBar(
+              content: Text("Transaction Sucessful!"),
+              backgroundColor: Colors.green,
+              duration: Duration(
+                seconds: 5,
+              ),
+            ),
+          );
+        }
+      },
+      onFailure: (err) => Scaffold.of(context).showSnackBar(snackBar_onFailure),
+      onClosed: () => Scaffold.of(context).showSnackBar(snackBar_onClosed),
+      context: context
+    );
+    _rave.process();
   }
 }
